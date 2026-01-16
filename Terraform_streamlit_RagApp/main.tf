1fe7b7fd62776a72e8708e065d9a14e297a6e9d9
applied-gen-ai <team@appliedgenaicourse.com>
2026-01-16 14:26:49 +0530

    llm-target change

--- Terraform_streamlit_RagApp/main.tf
+++ Terraform_streamlit_RagApp/main.tf
@@ -1,11 +1,3 @@
-module "vpc" {
-  source                = "./Modules/vpc"
-  vpc_cidr              = var.vpc_cidr
-  public_subnet_cidrs   = var.public_subnet_cidrs
-  private_subnet_cidrs  = var.private_subnet_cidrs
-  availability_zones    = var.availability_zones
-}
-
 module "iam" {
   source = "./Modules/iam"
   
